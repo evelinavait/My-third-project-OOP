3e530aa0c1e1f85619176058d658e58317365ad8
Evelina <72965991+evelinavait@users.noreply.github.com>
2022-10-14 08:19:37 +0300

    Update my_lib.hpp

--- my_lib.hpp
+++ my_lib.hpp
@@ -1,15 +1,10 @@
+#include <string.h>
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <cstdio>
 #include <vector>
-#include<stdio.h>
-#include<ctype.h>
+#include <sstream>
+#include <iomanip>
 #include <bits/stdc++.h>
-
+#include <ctime>
+#include <fstream>
 using std::vector;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
 using namespace std;
