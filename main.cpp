4095b9d10dc5d84ca9111adc7ed40f8961514976
Evelina <72965991+evelinavait@users.noreply.github.com>
2022-11-11 07:49:08 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "student.h"
+
+int main()
+{
+    int c;
+    srand(time(0));
+
+    cin.exceptions(std::istream::failbit);
+    try
+    {
+        cout << "Iveskite studentu kieki: ";
+        cin >> c;
+        GetStudents(c);
+    }
+
+
+    catch(const ios_base::failure& e)
+    {
+        cout << "Netinkamai ivestas studentu kiekis" << endl;
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}
